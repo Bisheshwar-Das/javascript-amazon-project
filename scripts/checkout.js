5f978a4ad96f555216d5ee547174aaf59ea1cc1c
Bisheshwar-Das <141819955+Bisheshwar-Das@users.noreply.github.com>
2025-02-06 21:32:32 +1100

    delete products form html when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -11,7 +11,7 @@ cart.forEach((cartItem)=>{
   });
 
 
-  cartSummaryHTML+=`<div class="cart-item-container">
+  cartSummaryHTML+=`<div class="cart-item-container js-cart-item-container-${productId}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -94,5 +94,8 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
   link.addEventListener('click',()=>{
     const productId=link.dataset.productId;
     removeFromCart(productId);
+
+    const container=document.querySelector(`.js-cart-item-container-${productId}`);
+    container.remove();
   })
 })
